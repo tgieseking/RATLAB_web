55ed652a695f35e174c872c74d29c68c1f9e4526
Timothy Gieseking <tgieseking2.71@gmail.com>
2017-11-05 23:16:54 -0500

    Markers added to map

--- js/controllers/sightingsMap.js
+++ js/controllers/sightingsMap.js
@@ -1,10 +1,23 @@
-ratlab.controller('SightingsMapController', ['$scope',
-  function($scope) {
+ratlab.controller('SightingsMapController', ['$scope', 'DatabaseService',
+  function($scope, DatabaseService) {
     var map;
     $scope.initMap =  function() {
       map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.730610, lng: -73.935242},
         zoom: 10
+      });
+      var sightingsList = DatabaseService.sightings;
+      sightingsList.$loaded().then(function(){
+        console.log(sightingsList.length);
+        for (i = 0; i < sightingsList.length; i++) {
+          var marker = new google.maps.Marker({
+            position: {
+              lat: sightingsList[i].latitude,
+              lng: sightingsList[i].longitude
+            },
+            map: map
+          });
+        }
       });
     }
   }
